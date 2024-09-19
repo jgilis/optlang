4c8da9c0d5fa0e8f907536a9f5757ade398081c6
jgilis <jeroen.gilis@ugent.be>
2024-09-19 13:36:39 +0200

    check tolerance key

--- src/optlang/glpk_interface.py
+++ src/optlang/glpk_interface.py
@@ -398,9 +398,10 @@ class Configuration(interface.MathematicalProgrammingConfiguration, metaclass=in
         for key, val in state.items():
             if key != "tolerances":
                 setattr(self, key, val)
-        for key, val in state["tolerances"].items():
-            if key in self._tolerance_functions():
-                setattr(self.tolerances, key, val)
+        if "tolerances" in state:
+            for key, val in state["tolerances"].items():
+                if key in self._tolerance_functions():
+                    setattr(self.tolerances, key, val)
 
     def _set_presolve(self, value):
         self._smcp.presolve = {False: GLP_OFF, True: GLP_ON, "auto": GLP_OFF}[value]
